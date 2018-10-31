d6dd46cae916762dd775b451ee8c363702c94867
LucasJoestar <lucasjoestar@outlook.fr>
2018-10-31 18:01:37 +0100

    Attendez, j'ai oublié un script

--- Assets/Prototypes/Scripts/PlayersPrototype/Throwavles & Trajectories/LineRendererExtension.cs
+++ Assets/Prototypes/Scripts/PlayersPrototype/Throwavles & Trajectories/LineRendererExtension.cs
@@ -6,6 +6,11 @@ using UnityEngine;
 [Serializable]
 public static class LineRendererExtension
 {
+    /// <summary>
+    /// Draws a 
+    /// </summary>
+    /// <param name="_value"></param>
+    /// <param name="_positions"></param>
     public static void DrawTrajectory(this LineRenderer _value, Vector3[] _positions)
     {
 
