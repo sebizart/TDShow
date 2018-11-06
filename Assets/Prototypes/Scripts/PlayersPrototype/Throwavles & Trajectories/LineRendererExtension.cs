8dfc6b16da3b882e372357631990d295357881fd
LucasJoestar <lucasjoestar@outlook.fr>
2018-11-06 09:22:10 +0100

    Revert "Merge branch 'Lucas-Zone' of https://github.com/sebizart/TDShow into Lucas-Zone"
    This reverts commit 84e4276321647c85d4dc1b1ef5f3bbcb8d21f851, reversing
    changes made to d7ce845d73acce44e7b1a7a78c212f40a1407af7.

--- Assets/Prototypes/Scripts/PlayersPrototype/Throwavles & Trajectories/LineRendererExtension.cs
+++ Assets/Prototypes/Scripts/PlayersPrototype/Throwavles & Trajectories/LineRendererExtension.cs
@@ -7,18 +7,12 @@ using UnityEngine;
 public static class LineRendererExtension
 {
     /// <summary>
-    /// Draws a trajectory passing by an array of positions
+    /// Draws a 
     /// </summary>
-    /// <param name="_value">LineRenderer to use</param>
-    /// <param name="_positions">Positions to pass by</param>
+    /// <param name="_value"></param>
+    /// <param name="_positions"></param>
     public static void DrawTrajectory(this LineRenderer _value, Vector3[] _positions)
     {
-        if (_positions.Length > 2)
-        {
-            //_value.material.mainTextureScale = new Vector2(Vector3.Distance(_positions[0], _positions[1]), 1);
-        }
 
-        _value.positionCount = _positions.Length;
-        _value.SetPositions(_positions);
     }
 }
