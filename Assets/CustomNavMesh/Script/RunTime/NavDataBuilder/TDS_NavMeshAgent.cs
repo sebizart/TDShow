ad6792b797e3706b5f3d57153b3dfeb135abe48f
ThiebautAlexis <thiebautalexis01@gmail.com>
2018-11-23 16:33:42 +0100

    CustomNavMesh
    Implémentation du CustomNavMesh

---
+++ Assets/CustomNavMesh/Script/RunTime/NavDataBuilder/TDS_NavMeshAgent.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TDS_NavMeshAgent : MonoBehaviour
+{
+    [SerializeField] TDS_CustomNavPath currentPath;
+    [SerializeField] Transform target;
+    int indexTarget = 0; 
+
+    bool isMoving = false; 
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (TDS_NavMeshManager.Instance.CalculatePath(transform.position, target.position, currentPath))
+            {
+                isMoving = true;
+            }
+        }
+        if (!isMoving) return;
+        Move(); 
+    }
+
+    private void Move()
+    {
+        Vector3 _targetedPosition = currentPath.NavigationPoints[indexTarget].Position;
+        transform.position = Vector3.MoveTowards(transform.position, _targetedPosition , Time.deltaTime * 2); 
+        if(Vector3.Distance(transform.position, _targetedPosition ) <= .1f)
+        {
+            indexTarget++; 
+            if(indexTarget == currentPath.NavigationPoints.Count)
+            {
+                isMoving = false;
+                indexTarget = 0;
+                currentPath.NavigationPoints.Clear(); 
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red; 
+        for (int i = 0; i < currentPath.NavigationPoints.Count; i++)
+        {
+            Gizmos.DrawSphere(currentPath.NavigationPoints[i].Position, .2f); 
+        }
+        for (int i = 0; i < currentPath.NavigationPoints.Count - 1 ; i++)
+        {
+            Gizmos.DrawLine(currentPath.NavigationPoints[i].Position, currentPath.NavigationPoints[i + 1].Position); 
+        }
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(currentPath.StartPosition, .2f);
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(currentPath.EndPosition, .2f);
+
+    }
+}
---
+++ Assets/CustomNavMesh/Script/RunTime/NavDataRuntime/TDS_CustomNavPath.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq; 
+using UnityEngine;
+
+[Serializable]
+public class TDS_CustomNavPath
+{
+    [SerializeField] Vector3 startPosition; 
+    public Vector3 StartPosition { get { return startPosition; } }
+
+    [SerializeField] Vector3 endPosition; 
+    public Vector3 EndPosition { get { return endPosition; } }
+
+    [SerializeField] List<TDS_NavPoint> navigationPoints = new List<TDS_NavPoint>(); 
+    public List<TDS_NavPoint> NavigationPoints
+    {
+        get
+        {
+            return navigationPoints;
+        }
+        set
+        {
+            navigationPoints = value;
+        }
+    }
+
+    public void BuildPath(Dictionary<TDS_NavPoint, TDS_NavPoint> _pathToBuild)
+    {
+        startPosition = _pathToBuild.First().Key.Position;
+        endPosition = _pathToBuild.Last().Value.Position;
+        TDS_NavPoint _currentPoint = _pathToBuild.Last().Key;
+        while (_currentPoint != _pathToBuild.First().Key)
+        {
+            navigationPoints.Add(_currentPoint);
+            _currentPoint = _pathToBuild[_currentPoint]; 
+        }
+        navigationPoints.Add(_currentPoint);
+        navigationPoints.Reverse(); 
+    }
+
+}
